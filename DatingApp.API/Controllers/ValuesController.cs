147f1f02d5327fd1b83d4f0e01843ec9b4b19227
WORKPC\Ralph_Fonz <raphael@fonseca.tech>
2018-03-27 13:46:19 -0400

    removed exception

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -13,8 +13,7 @@ namespace DatingApp.Api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            //return new string[] { "value1", "value2" };
-            throw new Exception("Test Exception");
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
